b412ce087fb04c156fafe49cfc4cf1c654ffc145
Poscat <poscat@mail.poscat.moe>
2020-10-29 18:12:00 +0800

    Simplify update script for vscode-insiders

--- pkgs/vscode-insiders/update.sh
+++ pkgs/vscode-insiders/update.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env nix-shell
 #!nix-shell -i bash -p nixFlakes jq curl
 cd "$(pwd)/$(dirname ${BASH_SOURCE})"
-new_url="$(curl -LsI -o /dev/null -w %{url_effective} https://vscode-update.azurewebsites.net/latest/linux-x64/insider)"
+url="https://vscode-update.azurewebsites.net/latest/linux-x64/insider"
+new_url="$(curl -LsI -o /dev/null -w %{url_effective} $url)"
 echo "{}" | jq --arg url  "$new_url" \
-               --arg sha256 "$(nix-prefetch-url https://vscode-update.azurewebsites.net/latest/linux-x64/insider)" \
+               --arg sha256 "$(nix-prefetch-url $url)" \
                '.url = $url | .sha256 = $sha256' > source.json
 echo "$new_url" | grep -Po '(?<=-)[0-9]+(?=.)' | tr -d '\n' > version
-ver="$(cat version)"
 git add .
-git commit -m "vscode-insiders: Auto updated to ${ver}" || echo "No changes to commit"
+git commit -m "vscode-insiders: Auto updated to $(cat version)" || echo "No changes to commit"
