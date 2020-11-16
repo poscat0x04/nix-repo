2a2834d48168cc185d4b76f7e3d2da9efeeee9d3
Poscat <poscat@mail.poscat.moe>
2020-11-16 17:15:55 +0800

    Auto update inputs when running actions

--- update.sh
+++ update.sh
@@ -5,3 +5,12 @@ for f in `find -mindepth 2 -name update.sh`; do
   echo "running update script in $dir ... "
   "$f"
 done
+
+echo "updating lock file"
+update=$(nix flake update . --recreate-lock-file 2>&1 | grep Updated)
+git add flake.lock
+git commit -m "flake.lock: Auto update
+
+Input changes:
+
+$update" || echo "No changes to commit"
