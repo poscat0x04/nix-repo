b0242f363981a3aad0b8779406cf1e53d6d761c6
Poscat <poscat@mail.poscat.moe>
2020-12-08 15:30:25 +0800

    vscode-extensions: add vscode-wakatime

--- pkgs/vscode-extensions/default.nix
+++ pkgs/vscode-extensions/default.nix
@@ -1,4 +1,4 @@
-{ vscode-utils, stdenv, ... }:
+{ vscode-utils, stdenv, wakatime, ... }:
 
 with stdenv.lib;
 with builtins; with attrsets; with lists; with vscode-utils;
@@ -6,6 +6,16 @@ with builtins; with attrsets; with lists; with vscode-utils;
 let
   extensions = fromJSON (readFile ./extensions.json);
   mergeList = foldr (x: xs: recursiveUpdate xs x) { };
+  buildExt =
+    ext: if ext.name == "vscode-wakatime"
+         then buildVscodeMarketplaceExtension {
+                mktplcRef = ext;
+                postPatch = ''
+                  mkdir -p wakatime-cli
+                  ln -s ${wakatime}/bin/wakatime wakatime-cli/wakatime-cli
+                '';
+              }
+         else extensionFromVscodeMarketplace ext;
 in
-  mergeList (map (ext: { "${ext.publisher}"."${ext.name}" = extensionFromVscodeMarketplace ext; }) extensions)
+  mergeList (map (ext: { "${ext.publisher}"."${ext.name}" = buildExt ext; }) extensions)
 
