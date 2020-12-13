38a039d375f0a9b15cdc7b3a5ef73352f384c006
Poscat <poscat@mail.poscat.moe>
2020-12-13 11:31:56 +0800

    fcitx5-material-color: fix build script

--- pkgs/fcitx5-material-color/default.nix
+++ pkgs/fcitx5-material-color/default.nix
@@ -9,14 +9,14 @@ stdenv.mkDerivation rec {
   src = fetchgit (fromJSON (readFile ./source.json));
 
   installPhase = ''
-    install -Dm644 arrow.png radio.png -t $out/usr/share/${pname}/
+    install -Dm644 arrow.png radio.png -t $out/share/${pname}/
     for variant in blue brown deepPurple indigo pink red teal; do
       variant_name=Material-Color-''${variant^}
-      install -Dm644 panel-$variant.png -t $out/usr/share/fcitx5/themes/$variant_name/
-      ln -s ../../../${pname}/arrow.png $out/usr/share/fcitx5/themes/$variant_name/
-      ln -s ../../../${pname}/radio.png $out/usr/share/fcitx5/themes/$variant_name/
-      install -Dm644 theme-$variant.conf $out/usr/share/fcitx5/themes/$variant_name/theme.conf
-      sed -i "s/^Name=.*/Name=$variant_name/" $out/usr/share/fcitx5/themes/$variant_name/theme.conf
+      install -Dm644 panel-$variant.png -t $out/share/fcitx5/themes/$variant_name/
+      ln -s ../../../${pname}/arrow.png $out/share/fcitx5/themes/$variant_name/
+      ln -s ../../../${pname}/radio.png $out/share/fcitx5/themes/$variant_name/
+      install -Dm644 theme-$variant.conf $out/share/fcitx5/themes/$variant_name/theme.conf
+      sed -i "s/^Name=.*/Name=$variant_name/" $out/share/fcitx5/themes/$variant_name/theme.conf
     done
   '';
 }
