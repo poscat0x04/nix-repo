02ca1ddbfa66e16b718bd0a27e8218f0f5843483
actions <actions@github.com>
2021-02-02 02:06:23 +0000

    firefox-addons: Auto update

--- pkgs/firefox-addons/addons.nix
+++ pkgs/firefox-addons/addons.nix
@@ -1,4 +1,4 @@
-{ buildFirefoxXpiAddon, fetchurl, stdenv }:
+{ buildFirefoxXpiAddon, fetchurl, lib, stdenv }:
   {
     "absolute-enable-right-click" = buildFirefoxXpiAddon {
       pname = "absolute-enable-right-click";
@@ -6,7 +6,7 @@
       addonId = "{9350bc42-47fb-4598-ae0f-825e3dd9ceba}";
       url = "https://addons.mozilla.org/firefox/downloads/file/1205179/absolute_enable_right_click_copy-1.3.8-fx.xpi";
       sha256 = "d1ca76d23234e6fd0f5d521caef62d20d071c8806887cda89914fd8325715a0a";
-      meta = with stdenv.lib;
+      meta = with lib;
       {
         description = "Force Enable Right Click &amp; Copy";
         platforms = platforms.all;
@@ -18,7 +18,7 @@
       addonId = "{84601290-bec9-494a-b11c-1baa897a9683}";
       url = "https://addons.mozilla.org/firefox/downloads/file/3525464/ctrlnumber_to_switch_tabs-1.0.1-an+fx.xpi";
       sha256 = "ce31b253e77c0646347ac0a80f43b0d16d6c8c896facc3b1e360dcc0bd6ce0f7";
-      meta = with stdenv.lib;
+      meta = with lib;
       {
         homepage = "https://github.com/AbigailBuccaneer/firefox-ctrlnumber";
         description = "Adds keyboard shortcut Ctrl+1 to switch to the first tab, Ctrl+2 to switch to the second, and so on. Ctrl+9 switches to the last tab.";
@@ -32,7 +32,7 @@
       addonId = "floccus@handmadeideas.org";
       url = "https://addons.mozilla.org/firefox/downloads/file/3718398/floccus-4.4.10-fx.xpi";
       sha256 = "e88d8ffed51f09fcfce9a18a2293e2589f517877a080b9cfe3d68da66842dbff";
-      meta = with stdenv.lib;
+      meta = with lib;
       {
         homepage = "https://floccus.org";
         description = "Sync your bookmarks across browsers via Nextcloud, WebDAV or a local file (and thus any file sync solution)";
@@ -46,7 +46,7 @@
       addonId = "87677a2c52b84ad3a151a4a72f5bd3c4@jetpack";
       url = "https://addons.mozilla.org/firefox/downloads/file/3713073/grammarly_for_firefox-8.869.0-an+fx.xpi";
       sha256 = "9cfcff8c723d1a2260db63e97ad74cc63fc086d20fb005ed8bef0881117ca3af";
-      meta = with stdenv.lib;
+      meta = with lib;
       {
         homepage = "http://grammarly.com";
         description = "Grammarly’s writing assistant has you covered in any writing situation. With real-time suggestions to help with grammar, clarity, tone, and more, you can be confident that your writing will make the impression you want.";
@@ -59,7 +59,7 @@
       addonId = "https-everywhere@eff.org";
       url = "https://addons.mozilla.org/firefox/downloads/file/3716461/https_everywhere-2021.1.27-an+fx.xpi";
       sha256 = "da049748bba7282c0f8c0ab85ac8f494e795e79d6bdc6f9f726d687aa8cc2a1f";
-      meta = with stdenv.lib;
+      meta = with lib;
       {
         homepage = "https://www.eff.org/https-everywhere";
         description = "Encrypt the web! HTTPS Everywhere is a Firefox extension to protect your communications by enabling HTTPS encryption automatically on sites that are known to support it, even when you type URLs or follow links that omit the https: prefix.";
@@ -78,7 +78,7 @@
       addonId = "jid1-KKzOGWgsW3Ao4Q@jetpack";
       url = "https://addons.mozilla.org/firefox/downloads/file/3707490/i_dont_care_about_cookies-3.2.7-an+fx.xpi";
       sha256 = "d5f917408c64a254694d34ffa14c48edd5c3fc9f9d2ab45459cf7236acc9352d";
-      meta = with stdenv.lib;
+      meta = with lib;
       {
         homepage = "https://www.i-dont-care-about-cookies.eu/";
         description = "Get rid of cookie warnings from almost all websites!";
@@ -92,7 +92,7 @@
       addonId = "{6d85dea2-0fb4-4de3-9f8c-264bce9a2296}";
       url = "https://addons.mozilla.org/firefox/downloads/file/671858/link_cleaner-1.5-an+fx.xpi";
       sha256 = "1ecec8cbe78b4166fc50da83213219f30575a8c183f7a13aabbff466c71ce560";
-      meta = with stdenv.lib;
+      meta = with lib;
       {
         homepage = "https://github.com/idlewan/link_cleaner";
         description = "Clean URLs that are about to be visited:\n- removes utm_* parameters\n- on item pages of aliexpress and amazon, removes tracking parameters\n- skip redirect pages of facebook, steam and reddit";
@@ -106,7 +106,7 @@
       addonId = "{6706d386-2d33-4e1e-bbf1-51b9e1ce47e1}";
       url = "https://addons.mozilla.org/firefox/downloads/file/3599813/pixiv_toolkit-4.2.1-fx.xpi";
       sha256 = "db60ca7cadfe1fa4b48a11ea613863a29e25a411cdf86079c6b74d6d7bd8efca";
-      meta = with stdenv.lib;
+      meta = with lib;
       {
         homepage = "https://github.com/leoding86/webextension-pixiv-toolkit";
         description = "Convert ugoira to gif or/and webm, pack manga for downloading and download novel on Pixiv.";
@@ -119,7 +119,7 @@
       addonId = "plasma-browser-integration@kde.org";
       url = "https://addons.mozilla.org/firefox/downloads/file/3715245/plasma_integration-1.8.0.1-fx.xpi";
       sha256 = "226f0931a139346d78d0d8ae950688a731cb07a3e430b3b3f779585ae33714e7";
-      meta = with stdenv.lib;
+      meta = with lib;
       {
         homepage = "http://kde.org";
         description = "Multitask efficiently by controlling browser functions from the Plasma desktop.";
@@ -133,7 +133,7 @@
       addonId = "jid1-MnnxcxisBPnSXQ@jetpack";
       url = "https://addons.mozilla.org/firefox/downloads/file/3691765/privacy_badger-2020.12.10-an+fx.xpi";
       sha256 = "635ded379b5c3259d7bf0997689215bcb0f1bbb77f9289f33b0c01b0cddc7ff0";
-      meta = with stdenv.lib;
+      meta = with lib;
       {
         homepage = "https://privacybadger.org/";
         description = "Automatically learns to block invisible trackers.";
@@ -147,7 +147,7 @@
       addonId = "{c5f00c00-e4bc-414d-9784-933857514bd0}";
       url = "https://addons.mozilla.org/firefox/downloads/file/3678406/save_to_the_wayback_machine-5.4.9-fx.xpi";
       sha256 = "3687b4985e9409cfe4c1043d5d8c785d205cfd4a56691cee38842232ab823c8e";
-      meta = with stdenv.lib;
+      meta = with lib;
       {
         homepage = "https://github.com/VerifiedJoseph/Save-to-the-Wayback-Machine";
         description = "Quickly save web pages to the Internet Archive's Wayback Machine and see the last time your current page was archived.";
@@ -161,7 +161,7 @@
       addonId = "jid1-0GMKQAI8A9x5jg@jetpack";
       url = "https://addons.mozilla.org/firefox/downloads/file/3042549/transmission_easy_client-2.2.0-fx.xpi";
       sha256 = "2beb4ce0dcc24ca8b45c222896e2e01d397ab17a2adcbf708e9e80062af4d65f";
-      meta = with stdenv.lib;
+      meta = with lib;
       {
         description = "Transmission easy client - это дополнение позволяет легко интегрировать Transmission в браузер!";
         license = licenses.mpl20;
@@ -174,7 +174,7 @@
       addonId = "uBlock0@raymondhill.net";
       url = "https://addons.mozilla.org/firefox/downloads/file/3719054/ublock_origin-1.33.2-an+fx.xpi";
       sha256 = "5c3a5ef6f5b5475895053238026360020d6793b05541d20032ea9dd1c9cae451";
-      meta = with stdenv.lib;
+      meta = with lib;
       {
         homepage = "https://github.com/gorhill/uBlock#ublock-origin";
         description = "Finally, an efficient wide-spectrum content blocker. Easy on CPU and memory.";
