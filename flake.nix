40d9709d84b63b413ff577c33fbfcbb7a8911518
Poscat <poscat@mail.poscat.moe>
2021-08-17 08:48:56 +0800

    v2ray-rules-dat: rename .dat files

--- flake.nix
+++ flake.nix
@@ -23,8 +23,8 @@
           nord-dircolors = callPackage ./pkgs/nord-dircolors { };
           nord-konsole = callPackage ./pkgs/nord-konsole { };
           v2ray-rules-dat = {
-            geoip = callPackage ./pkgs/v2ray-rules-dat/geoip.nix { };
-            geosite = callPackage ./pkgs/v2ray-rules-dat/geosite.nix { };
+            "geoip.dat" = callPackage ./pkgs/v2ray-rules-dat/geoip.nix { };
+            "geosite.dat" = callPackage ./pkgs/v2ray-rules-dat/geosite.nix { };
           };
         };
         firefox-addons = callPackages ./pkgs/firefox-addons { };
