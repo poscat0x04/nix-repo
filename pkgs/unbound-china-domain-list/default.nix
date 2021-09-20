e697275826f548ecbe9ba414fda9f15462ade508
Poscat <poscat@mail.poscat.moe>
2021-09-20 21:31:57 +0800

    unbound-china-domain-list: support multiple servers

--- pkgs/unbound-china-domain-list/default.nix
+++ pkgs/unbound-china-domain-list/default.nix
@@ -1,8 +1,13 @@
-{ stdenv, fetchgit, server ? "223.5.5.5" }:
+{ stdenv
+, lib
+, fetchgit
+, servers ? [ "223.5.5.5" "223.6.6.6" "119.29.29.29" "119.28.28.28" ]
+}:
 
 with builtins;
-
-stdenv.mkDerivation {
+let
+  server-str = "[ " + lib.concatMapStringsSep " " (server: "\"${server}\"") servers + " ]";
+in stdenv.mkDerivation {
   pname = "unbound-china-domain-list";
 
   version = builtins.readFile ./version;
@@ -14,7 +19,7 @@ stdenv.mkDerivation {
   buildPhase = ''
     echo "[" >> $out
     for f in apple google accelerated-domains
-    do sed -r 's|server=/(.*)/114.114.114.114$|{ name = "\1."; forward-addr = "${server}"; }|' $src/$f.china.conf >> $out
+    do sed -r 's|server=/(.*)/114.114.114.114$|{ name = "\1."; forward-addr = ${server-str}; }|' $src/$f.china.conf >> $out
     done
     echo "]" >> $out
   '';
