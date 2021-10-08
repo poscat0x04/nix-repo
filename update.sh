0da11f6bf52988501a4c78c4c3d2517dfa1d7357
Poscat <poscat@mail.poscat.moe>
2021-10-08 13:55:43 +0800

    cleanup

--- update.sh
+++ update.sh
@@ -3,13 +3,3 @@ for f in pkgs/**/update.sh; do
   echo "running update script in $dir ... "
   "$f"
 done
-
-echo "Uploading to cachix"
-
-packages=(fcitx5-material-color)
-
-for p in "${packages[@]}"
-do
-  echo "Building ${p}"
-  nix build -L --json ".#${p}" | jq -r ".[0].outputs.out" | cachix push nix-repo
-done
