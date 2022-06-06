d82febe29925bcc9bbec0cbf0a8f0ee94e707cd9
actions <actions@github.com>
2022-06-06 08:25:33 +0000

    firefox-addons: Auto update

--- pkgs/firefox-addons/addons.nix
+++ pkgs/firefox-addons/addons.nix
@@ -199,10 +199,10 @@
       };
     "sponsorblock" = buildFirefoxXpiAddon {
       pname = "sponsorblock";
-      version = "4.4.5";
+      version = "4.5.1";
       addonId = "sponsorBlocker@ajay.app";
-      url = "https://addons.mozilla.org/firefox/downloads/file/3954023/sponsorblock-4.4.5.xpi";
-      sha256 = "813d393b7997ec350a23deacef9c3f707c553abc32cf0ed2a4023edfb09d02ed";
+      url = "https://addons.mozilla.org/firefox/downloads/file/3958238/sponsorblock-4.5.1.xpi";
+      sha256 = "8c63c761066de4da1c7c51fd4f66ea42a7c54c8f38e0d02ec427a24025c5f404";
       meta = with lib;
       {
         homepage = "https://sponsor.ajay.app";
