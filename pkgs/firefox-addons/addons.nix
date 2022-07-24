debebd1f50ed42e02fc24db7b6915fef63256a8d
actions <actions@github.com>
2022-07-24 17:23:59 +0000

    firefox-addons: Auto update

--- pkgs/firefox-addons/addons.nix
+++ pkgs/firefox-addons/addons.nix
@@ -28,10 +28,10 @@
       };
     "bypass-paywalls-clean" = buildFirefoxXpiAddon {
       pname = "bypass-paywalls-clean";
-      version = "2.7.6.0";
+      version = "2.7.7.0";
       addonId = "{d133e097-46d9-4ecc-9903-fa6a722a6e0e}";
-      url = "https://addons.mozilla.org/firefox/downloads/file/3976892/bypass_paywalls_clean-2.7.6.0.xpi";
-      sha256 = "9bc186e25bfebee2caef50ed578d1d5edc206d6c86eb655f48424a620eeb3e98";
+      url = "https://addons.mozilla.org/firefox/downloads/file/3979537/bypass_paywalls_clean-2.7.7.0.xpi";
+      sha256 = "2ad514f3075a561d76e0668973901d87829253b010931788578387a47ca0bc91";
       meta = with lib;
       {
         homepage = "https://gitlab.com/magnolia1234/bypass-paywalls-firefox-clean";
