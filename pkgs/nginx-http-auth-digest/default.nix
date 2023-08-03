bc8b470f836424761fc6068682c36e0704cddf10
poscat <poscat@poscat.moe>
2023-08-03 10:56:47 +0800

    nginx-http-auth-digest: add meta

--- pkgs/nginx-http-auth-digest/default.nix
+++ pkgs/nginx-http-auth-digest/default.nix
@@ -5,4 +5,9 @@ with builtins;
 {
   name = "http-auth-digest";
   src = fetchgit (fromJSON (readFile ./source.json));
+  meta = {
+    description = "Nginx Digest Authentication module";
+    homepage = "https://github.com/atomx/nginx-http-auth-digest";
+    license = [];
+  };
 }
