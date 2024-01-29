43fe684a286e01450ee76f6333a038dc8e81c458
actions <actions@github.com>
2024-01-29 18:19:50 +0000

    firefox-addons: Auto update

--- pkgs/firefox-addons/addons.nix
+++ pkgs/firefox-addons/addons.nix
@@ -764,7 +764,7 @@
       meta = with lib;
       {
         homepage = "https://sponsor.ajay.app";
-        description = "Easily skip YouTube video sponsors. When you visit a YouTube video, the extension will check the database for reported sponsors and automatically skip known sponsors. You can also report sponsors in videos.\n\nOther browsers: https://sponsor.ajay.app";
+        description = "Easily skip YouTube video sponsors. When you visit a YouTube video, the extension will check the database for reported sponsors and automatically skip known sponsors. You can also report sponsors in videos. Other browsers: https://sponsor.ajay.app";
         license = licenses.lgpl3;
         mozPermissions = [
           "storage"
